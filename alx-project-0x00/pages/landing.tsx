d7f4b9a56b44771a5f0f5d0ae80b8454d80d0bdd
Ogidol <ogundele115@gmail.com>
2025-08-09 01:50:24 +0100

    first commit

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -6,8 +6,9 @@ const Landing: React.FC = () => {
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
-      <button styles="rounded-sm">Rounded Small</button>
+      <Button styles="rounded-sm">Rounded Small</Button>
       <Button styles="rounded-md">Rounded Medium</Button>
+      <Button styles="rounded-lg">Rounded Medium</Button>
       <Button styles="rounded-full">Rounded Full</Button>
     </div>
   );
